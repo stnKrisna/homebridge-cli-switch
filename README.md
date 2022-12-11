77da4438927324f20715b9bce40fe4dc2e9820a0
Stanislaus Krisna <krisna.putra456789@gmail.com>
2022-12-11 15:15:15 +1100

    Execute command directly instead of SSHing

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-homebridge-ssh
-==============
+homebridge-cli-switch
+=====================
 
-Supports triggering ssh commands on the HomeBridge platform.
+A fork of [homebridge-ssh](https://github.com/zb0th/homebridge-ssh).
+
+Supports triggering CLI commands on the HomeBridge platform.
 
 ## Installation
 
 1. Install homebridge using: `npm install -g homebridge`
-2. Install this plugin using: `npm install -g homebridge-ssh`
+2. Install this plugin using: `npm install -g homebridge-cli-switch`
 3. Update your configuration file. See `sample-config.json` in this repository for a sample.
 
 ## Configuration
@@ -16,20 +18,14 @@ Configuration sample:
 ```
 "accessories": [
 	{
-              "accessory": "SSH",
-              "name": "iTunes Music",
-              "on": "osascript -e 'tell application \"iTunes\" to play'",
-              "off": "osascript -e 'tell application \"iTunes\" to stop'",
-              "state": "osascript -e 'tell application \"iTunes\" to get player state'",
-              "on_value" : "playing",
-              "exact_match": true,
-              "ssh": {
-                "user": "me",
-                "host": "mymac",
-                "port": 22,
-                "password": "password (or use key)",
-                "key": "path to private key file"
-              }
+		"accessory": "CLI",
+		"name": "Bedroom TV",
+		"on": "caffeinate -u -t 5",
+		"off": "pmset displaysleepnow",
+		"state": "ioreg -n IODisplayWrangler | grep -i IOPowerManagement | perl -pe 's/^.*DevicePowerState\"=([0-9]+).*$/\\1/'",
+		"on_value": "4",
+		"exact_match": true,
+		"exec_timeout": 5000
 	}
 ]
 ```
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var Service;
 var Characteristic;
 
-var ssh = require('ssh-exec'),
-    assign = require('object-assign');
+var assign = require('object-assign');
+var exec = require('child_process').exec;
+
+const DEFAULT_TIMEOUT = 5000;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-ssh', 'SSH', SshAccessory);
+  homebridge.registerAccessory('homebridge-cli', 'CLI', CliAccessory);
 }
 
-function SshAccessory(log, config) {
+function CliAccessory(log, config) {
   this.log = log;
   this.service = 'Switch';
 
@@ -21,71 +23,83 @@ function SshAccessory(log, config) {
   this.onValue = config['on_value'] || "playing";
   this.onValue = this.onValue.trim().toLowerCase();
   this.exactMatch = config['exact_match'] || true;
-  this.ssh = assign({
-    user: config['user'],
-    host: config['host'],
-    password: config['password'],
-    key: config['key']
-  }, config['ssh']);
+  this.execTimeout = config['exec_timeout'] || DEFAULT_TIMEOUT;
 }
 
-SshAccessory.prototype.matchesString = function(match) {
-  if(this.exactMatch) {
-    return (match === this.onValue);
+function execCommand(command, timeout) {
+  if (Number.isNaN(timeout)) {
+    timeout = DEFAULT_TIMEOUT
   }
-  else {
+
+  return new Promise((resolve, reject) => {
+    exec(command, {
+      timeout
+    }, (error, stdout, stderr) => {
+      if (error) {
+        reject({
+          error,
+          stderr
+        });
+        return;
+      }
+
+      resolve(stdout);
+    });
+  })
+}
+
+CliAccessory.prototype.matchesString = function(match) {
+  if (this.exactMatch) {
+    return (match === this.onValue);
+  } else {
     return (match.indexOf(this.onValue) > -1);
   }
 }
 
-SshAccessory.prototype.setState = function(powerOn, callback) {
+CliAccessory.prototype.setState = function(powerOn, callback) {
   var accessory = this;
   var state = powerOn ? 'on' : 'off';
   var prop = state + 'Command';
   var command = accessory[prop];
 
-  var stream = ssh(command, accessory.ssh);
-
-  stream.on('error', function (err) {
-    accessory.log('Error: ' + err);
-    callback(err || new Error('Error setting ' + accessory.name + ' to ' + state));
-  });
-
-  stream.on('finish', function () {
-    accessory.log('Set ' + accessory.name + ' to ' + state);
-    callback(null);
-  });
+  execCommand(command, this.execTimeout)
+    .then(() => {
+      accessory.log('Set ' + accessory.name + ' to ' + state);
+      callback(null);
+    })
+    .catch(error => {
+      accessory.log('Error: ' + error);
+      callback(error || new Error('Error setting ' + accessory.name + ' to ' + state));
+    });
 }
 
-SshAccessory.prototype.getState = function(callback) {
+CliAccessory.prototype.getState = function(callback) {
   var accessory = this;
   var command = accessory['stateCommand'];
 
-  var stream = ssh(command, accessory.ssh);
-
-  stream.on('error', function (err) {
-    accessory.log('Error: ' + err);
-    callback(err || new Error('Error getting state of ' + accessory.name));
-  });
-
-  stream.on('data', function (data) {
-    var state = data.toString('utf-8').trim().toLowerCase();
-    accessory.log('State of ' + accessory.name + ' is: ' + state);
-    callback(null, accessory.matchesString(state));
-  });
+  execCommand(command, this.execTimeout)
+    .then((stdout) => {
+      var state = stdout.toString('utf-8').trim().toLowerCase();
+      accessory.log('State of ' + accessory.name + ' is: ' + state);
+      callback(null, accessory.matchesString(state));
+    })
+    .catch(error => {
+      accessory.log('Error: ' + error);
+      callback(error || new Error('Error getting state of ' + accessory.name));
+    });
 }
 
-SshAccessory.prototype.getServices = function() {
+CliAccessory.prototype.getServices = function() {
   var informationService = new Service.AccessoryInformation();
   var switchService = new Service.Switch(this.name);
 
   informationService
-  .setCharacteristic(Characteristic.Manufacturer, 'SSH Manufacturer')
-  .setCharacteristic(Characteristic.Model, 'SSH Model')
-  .setCharacteristic(Characteristic.SerialNumber, 'SSH Serial Number');
+    .setCharacteristic(Characteristic.Manufacturer, 'CLI Manufacturer')
+    .setCharacteristic(Characteristic.Model, 'CLI Model')
+    .setCharacteristic(Characteristic.SerialNumber, 'CLI Serial Number');
 
   var characteristic = switchService.getCharacteristic(Characteristic.On)
-  .on('set', this.setState.bind(this));
+    .on('set', this.setState.bind(this));
 
   if (this.stateCommand) {
     characteristic.on('get', this.getState.bind(this))
