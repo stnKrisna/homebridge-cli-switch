8ac14c31525cd13fed6f14ba4cbc2c540063d89f
Stanislaus Krisna <krisna.putra456789@gmail.com>
2022-12-16 12:02:46 +1100

    Code cleanup

---
+++ CliAccessory.js
@@ -0,0 +1,124 @@
+const DEFAULT_TIMEOUT = 5000;
+
+let Service;
+let Characteristic;
+
+class CliAccessory {
+  constructor(log, config, homebridge, execFunc = null) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+
+    this.log = log;
+    this.service = 'Switch';
+
+    this.name = config['name'];
+    this.onCommand = config['on'];
+    this.offCommand = config['off'];
+    this.stateCommand = config['state'];
+    this.onValue = config['on_value'] || "playing";
+    this.onValue = this.onValue.trim().toLowerCase();
+    this.exactMatch = config['exact_match'] || true;
+    this.execTimeout = config['exec_timeout'] || DEFAULT_TIMEOUT;
+
+    this.exec = execFunc === null ?
+      require('child_process').exec :
+      execFunc;
+  }
+
+  execCommand(command, timeout) {
+    if (Number.isNaN(timeout)) {
+      timeout = DEFAULT_TIMEOUT
+    }
+
+    return new Promise((resolve, reject) => {
+      this.exec(command, {
+        timeout
+      }, (error, stdout, stderr) => {
+        if (error) {
+          reject({
+            error,
+            stderr
+          });
+          return;
+        }
+
+        resolve(stdout);
+      });
+    })
+  }
+
+  matchesString(match) {
+    if (this.exactMatch) {
+      return (match === this.onValue);
+    } else {
+      return (match.indexOf(this.onValue) > -1);
+    }
+  }
+
+  setState(powerOn, callback) {
+    var accessory = this;
+    var state = powerOn ? 'on' : 'off';
+    var prop = state + 'Command';
+    var command = accessory[prop];
+
+    this.execCommand(command, this.execTimeout)
+      .then(() => {
+        accessory.log('Set ' + accessory.name + ' to ' + state);
+        callback(null);
+      })
+      .catch(error => {
+        accessory.log('Error: ' + error);
+        callback(error || new Error('Error setting ' + accessory.name + ' to ' + state));
+      });
+  }
+
+  getState(callback) {
+    var accessory = this;
+    var command = accessory['stateCommand'];
+
+    this.execCommand(command, this.execTimeout)
+      .then((stdout) => {
+        var state = stdout.toString('utf-8').trim().toLowerCase();
+        accessory.log('State of ' + accessory.name + ' is: ' + state);
+        callback(null, accessory.matchesString(state));
+      })
+      .catch(error => {
+        accessory.log('Error: ' + error);
+        callback(error || new Error('Error getting state of ' + accessory.name));
+      });
+  }
+
+  getAccessoryInfo () {
+    if (this.informationService === undefined) {
+      this.informationService = new Service.AccessoryInformation();
+      this.informationService
+        .setCharacteristic(Characteristic.Manufacturer, 'CLI Manufacturer')
+        .setCharacteristic(Characteristic.Model, 'CLI Model')
+        .setCharacteristic(Characteristic.SerialNumber, 'CLI Serial Number');
+    }
+
+    return this.informationService;
+  }
+
+  getAccessoryType () {
+    if (this.switchService === undefined) {
+      let switchService = new Service.Switch(this.name);
+      let characteristic = switchService.getCharacteristic(Characteristic.On)
+        .on('set', this.setState.bind(this));
+
+      if (this.stateCommand) {
+        characteristic.on('get', this.getState.bind(this))
+      };
+
+      this.switchService = switchService;
+    }
+
+    return this.switchService;
+  }
+
+  getServices() {
+    return [this.getAccessoryInfo(), this.getAccessoryType()];
+  }
+};
+
+module.exports = CliAccessory
